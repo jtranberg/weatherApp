7ea1ed98c328293f71d8cb5b73198a82452bb65a
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-12-06 20:14:09 -0800

    hooks connected, state change, with console log test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import axios from 'axios';
 //useEffect does something after rendering
 
 function App() {
+  const [search, setSearch] = useState('')
 
   const [allData, setAllData] = useState({
     city: '',
@@ -22,7 +23,7 @@ function App() {
   const fetchData = async (city) => {
     try{
    const APIKEY = '3e2a83f881e56be636d6f7106141094e'
-   const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${'Milan'}&appid=${APIKEY}`)
+   const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`)
    await setAllData({
     city: result.data.name,
     country:result.data.sys.country,
@@ -32,16 +33,39 @@ function App() {
     console.log('API not loaded correctly or loaded for first time')
     }
   }
+  const handleSubmit = (event) => {
+    console.log(search)
+    event.preventDefault()  
+    fetchData(search)
+  }
 
-
-  return (
+    const handleChange = (event) => {
+        setSearch(event.target.value)
+        }
+return (
+  <main>
     <div className="App">
-        {console.log("test: successful deployment",
-         ' 2. test for API fetch( city, country, temp)', 
-         ' 3. test for temp',  
-         allData.city,allData.country,  allData.temp)}
-        <h1>Check the Weather</h1>
-    </div>
+       <h1>Check the Weather</h1>
+       <form onSubmit={handleSubmit}>
+        <input
+        value={search}
+        type='text'
+        name='city'
+        placeholder='City Name'
+        onChange={handleChange}
+        />
+        <button for='city'>Search</button>
+      </form>
+          
+     <section>
+          <h1>{allData.city}</h1>
+          <h2>{allData.country}</h2>
+          <h3>Temperature  </h3>
+          <p>{allData.temp }°C</p>
+          <p>{allData.temp * 1.8 + 32}°F</p> {/*  convert  °C to °F* */}
+        </section>
+      </div>
+    </main>
   );
 }
 
