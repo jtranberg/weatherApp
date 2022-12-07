f2c526aa0343d30675d617952ec90aa7d9cdb4dd
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-12-06 16:09:05 -0800

    api functions and console testing pssed and complete

--- src/App.js
+++ src/App.js
@@ -16,19 +16,31 @@ function App() {
   })
 
   useEffect(() => {     //render api call
-        
-  })  
+       fetchData() 
+  },[]) 
 
   const fetchData = async (city) => {
+    try{
    const APIKEY = '3e2a83f881e56be636d6f7106141094e'
-   const result = await axios.get(`api.openweathermap.org/data/2.5/weather?q=${city }&appid=${APIKEY}`)
+   const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${'Milan'}&appid=${APIKEY}`)
+   await setAllData({
+    city: result.data.name,
+    country:result.data.sys.country,
+    temp: result.data.main.temp
+      })
+    } catch(e) {
+    console.log('API not loaded correctly or loaded for first time')
+    }
   }
 
 
   return (
     <div className="App">
-        {console.log('Hello World!!!!')}
-        <h1>Weather App</h1>
+        {console.log("test: successful deployment",
+         ' 2. test for API fetch( city, country, temp)', 
+         ' 3. test for temp',  
+         allData.city,allData.country,  allData.temp)}
+        <h1>Check the Weather</h1>
     </div>
   );
 }
